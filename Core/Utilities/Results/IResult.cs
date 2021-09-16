432ad3655149261acd8b51c8eee81cc02eb08cc1
Kursat Burhan Ozdemir <kursat82697@ogr.duzce.edu.tr>
2021-09-17 02:14:42 +0300

    Proje dosyası ekle.

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{//TEMEL VOIDLER ICIN BASLANGIC
+    public interface IResult
+    { bool Success { get; }
+
+       string Message { get; }
+    }
+}
---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Category:IEntity
+    {
+        //çıplak class kalmasın 
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+
+
+    }
+}
